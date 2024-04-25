28f9818a0051db2d84784acae133cdc75c04cc29
Srivats711 <srivatsmahesh7@gmail.com>
2024-04-25 16:50:13 +0530

    adding login and landing pages

--- website/app.py
+++ website/app.py
@@ -1,15 +1,18 @@
-from flask_cors import CORS
+# from flask_cors import CORS
 from flask import Flask,render_template
 
 
 app = Flask(__name__)
-cors = CORS(app,resources={r'/*':{'origin':'*'}})
+# cors = CORS(app,resources={r'/*':{'origin':'*'}})ṇ
 
 
 @app.route('/')
 def test():
     return render_template('index.html')
 
+@app.route('/login')
+def log():
+    return render_template('login.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
