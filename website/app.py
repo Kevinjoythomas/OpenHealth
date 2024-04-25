4401b5e5f2f3b5f32bd6699c8d93681a482fb093
Srivats711 <srivatsmahesh7@gmail.com>
2024-04-25 17:24:32 +0530

    demp from and demo route to test input and reponse using flask

--- website/app.py
+++ website/app.py
@@ -1,5 +1,5 @@
 # from flask_cors import CORS
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 
 app = Flask(__name__)
@@ -13,6 +13,17 @@ def test():
 @app.route('/login')
 def log():
     return render_template('login.html')
+
+@app.route('/test', methods=["POST", "GET"])
+def test1():
+    if(request.method=="POST"):
+      print('abcd')
+      name = request.form['name']
+      email = request.form['email']
+      return render_template('test1.html', name=name,email=email)
+    else:
+      print('abc')
+      return render_template('test.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
