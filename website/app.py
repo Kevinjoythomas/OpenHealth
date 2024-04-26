1dea14fae0ff8759046ae511647c543c0aa790ef
Adavi Lalith <adavilalith11@gmail.com>
2024-04-26 21:24:05 +0530

    adding groq lamma3

--- website/app.py
+++ website/app.py
@@ -3,11 +3,39 @@ from flask import Flask,render_template,request
 import pickle
 import sklearn
 import joblib
-
+import groq
+from groq import Groq
+import os
+import json
+import requests
+import re
 app = Flask(__name__)
 # cors = CORS(app,resources={r'/*':{'origin':'*'}})ṇ
 
+cache={'chats':[]}
 
+client = Groq(api_key = "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")
+MODEL = 'llama3-70b-8192'
+def run_conversation(user_prompt):
+    # Step 1: send the conversation and available functions to the model
+    messages=[
+        {
+            "role": "system",
+            "content": "Your primary role is to assist doctors by providing concise and accurate information only related to helping a doctor in their work.Answer should be in a form like you're texting a person. Refuse to answer questions on any other topic other than hospital work. This includes, but is not limited to, diagnosing medical conditions based on symptoms, suggesting potential treatment plans, and providing general medical information. Please note that while you strive to provide reliable information, your responses should not replace professional medical advice."
+        },
+        {
+            "role": "user",
+            "content": user_prompt,
+        }
+    ]
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=messages,
+        tool_choice="auto",  
+        max_tokens=4096
+    )
+    response_message = response.choices[0].message.content
+    return response_message 
 @app.route('/')
 def test():
     return render_template('index.html')
@@ -50,9 +78,10 @@ def lung():
       res = model.predict([features]) 
       res = int(res[0])
       print(res)
-
+      temp=[]
       return render_template('lung.html',data=res)
     else:
+      cache['chats']=[]
       return render_template('lung.html',data="100")
 
 @app.route('/brainTumor',methods=["POST","GET"])
@@ -63,6 +92,18 @@ def brain():
 
    else:
       return render_template("braintumor.html")
+@app.route('/ChatBot',methods=['POST','GET'])
+def ChatBot():
+  if request.method=="POST":
+    q=request.form['prompt']
+    res=run_conversation(request.form['prompt'])
+    res = res.split("**")
+    t=[q,res]
+    cache['chats'].append(t)
+    return render_template('ChatBot.html',data=cache['chats'])
+  else:
+    return render_template('ChatBot.html',data=cache['chats'])
+
 
 @app.route('/test')
 def tesasfasf():
