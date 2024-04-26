3394cc2b331e2d87da88327853e71fa1f983a87f
Adavi Lalith <adavilalith11@gmail.com>
2024-04-26 14:58:00 +0530

    pusing working verions of project

--- website/static/lung.js
+++ website/static/lung.js
@@ -2,16 +2,3 @@ document.querySelector('.card h2').addEventListener('click', function() {
     var form = document.getElementById('symptomsForm');
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
 });
-function previewImage() {
-    var file = document.getElementById("imageUpload").files[0];
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        var preview = document.getElementById("preview");
-        var imagePreviewDiv = document.getElementById("imagePreview");
-        preview.src = e.target.result;
-        imagePreviewDiv.style.display = 'block';
-    };
-    if (file) {
-        reader.readAsDataURL(file);
-    }
-}
