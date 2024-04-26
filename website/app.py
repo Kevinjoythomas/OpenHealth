39c9c658f700b3f7763c92d695b0d7d1aa654f78
KarthikeyaVoocha <voochakarthik@gmail.com>
2024-04-26 13:25:13 +0530

    BrainTumorSegemntation route

--- website/app.py
+++ website/app.py
@@ -26,9 +26,16 @@ def lung():
                  request.form["coughing"],
                  request.form["swallowing_difficulty"],
                  request.form["chest_pain"]]
-      return render_template('lung.html')
+      return render_template('lung.html',result="None")
     else:
       return render_template('lung.html')
+
+@app.route('/BrainTumorSegmentation',methods=["POST","GET"])
+def brain():
+   if(request.method=="POST"):
+      if 'image' in request.files:
+        image_file = request.files['image']
+      
 
 if __name__ == '__main__':
   app.run(debug=True)
